f5b1d5efa3da7103982f0eca5744417ac58c403f
vorujack <vorujack@gmail.com>
2019-11-09 10:52:49 +0330

    sobe bug fix and optimization on merged code

--- ErgoAccounting/settings.py
+++ ErgoAccounting/settings.py
@@ -70,10 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ErgoAccounting.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 try:
     from .production import *
 except:
@@ -115,3 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path , include
-from .views import *
 from rest_framework import routers
+
+from .views import *
 
 router = routers.DefaultRouter()
 router.register(r'shares', ShareView)
