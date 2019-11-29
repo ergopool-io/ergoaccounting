f3df273a7a01d7dc8ed65be04e81f4cded6de734
vorujack <vorujack@gmail.com>
2019-11-29 17:21:10 +0330

    remove code commented

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,5 @@ router.register(r'balance', BalanceView)
 router.register(r'conf', ConfigurationViewSet)
 router.register(r'dashboard', DashboardView, basename='Dashboard')
 
-urlpatterns = router.urls #+ [
-#     path('dashboard/', DashboardView.as_view()),
-#     path('dashboard/<str:pk>', DashboardView.as_view())
-# ]
+urlpatterns = router.urls
+
