551d2c012eae9c166f97defc80f2d287513223b3
AMO <arashmarioriyad@gmail.com>
2019-12-05 17:20:30 +0330

    create custom command for updating status of balances

---
+++ core/management/commands/convert_immature_to_mature.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from core.models import Balance, Share, Configuration
+
+
+NODE_URL = getattr(settings, "NODE_URL", "http://vorujak:9052/")
+url = os.path.join(NODE_URL, "wallet/transactionById")
+
+
+class Command(BaseCommand):
+    help = 'updating immature balances to mature balance'
+
+    def handle(self, *args, **kwargs):
+        shares = Share.objects.filter(balance__status=1)
+        for share in shares:
+            PARAMS = {"id": share.transaction_id}
+            transaction = requests.get(url, PARAMS).json()
+            number_of_confirmations = transaction["numConfirmations"]
+            height = transaction["inclusionHeight"]
+            if number_of_confirmations > Configuration.objects.CONFIGURATION_LENGTH:
+                Balance.objects.filter(share=share).update(status=2)
+                share.block_height = height
+                share.save()
+
