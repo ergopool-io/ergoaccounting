0deba00887e4ab278ae99c5b7bf9d95582ab0201
Moein Zargarzadeh <moein.zargarzadeh@gmail.com>
2020-08-05 21:56:04 +0430

    fixed ci

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sed -i "s/'HOST': 'db'/'HOST': 'postgres'/" ErgoAccounting/production.py
 coverage run --omit="*/migrations/*","*/wsgi.py","*/urls.py","*/settings.py","*/production.py" --source=core,ErgoAccounting manage.py test -v 2
 coverage report --fail-under=85
